11ba31c3cc1d738717c8ba8b166d1e5a27ad2aa3
alu2016394 <alvarosalomoni.97@gmail.com>
2018-10-30 18:34:31 +0100

    Initial Commit

---
+++ moneyExchange_Alvaro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  moneyExchange_Alvaro
+//
+//  Created by Alvaro Salomoni on 30/10/2018.
+//  Copyright © 2018 Alvaro Salomoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
